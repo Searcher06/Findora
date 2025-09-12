a3e099e99ca5f22074d9b12582512f00b418accd
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-09-12 02:46:47 +0100

    building the create item controller

--- server/src/controllers/item.controllers.js
+++ server/src/controllers/item.controllers.js
@@ -1,4 +1,52 @@
-const createItem = async (req, res) => {};
+import { textValidator } from "../utils/symbolchecker";
+
+const createItem = async (req, res) => {
+  const {
+    itemName,
+    itemDescription,
+    category,
+    location,
+    image,
+    status,
+    dateLostOrFound,
+  } = req.body;
+
+  // prettier-ignore
+  if(!itemName || !itemDescription || !category || !location || !image || !status || !dateLostOrFound){
+    res.status(400);
+    throw new Error("Please add fields!")
+  }
+
+  if (itemName.length < 3 || itemName.length > 25) {
+    res.status(400);
+    throw new Error(
+      "Item name must be atlease 3 characters long, 25 characters max"
+    );
+  }
+
+  if (itemDescription.length < 20 || itemDescription.length > 200) {
+    res.status(400);
+    throw new Error("Description must be between 20 to 200 characters");
+  }
+
+  if (location.length < 3 || location.length > 150) {
+    res.status(400);
+    throw new Error("Location must be between 10 to 150 characters");
+  } else if (textValidator(location)) {
+    res.status(400);
+    throw new Error("Location can not contain special characters");
+  }
+
+  if (dateLostOrFound > Date.now()) {
+    res.status(400);
+    throw new Error("Invalid date!");
+  }
+
+  // checking if theres an image to upload to claudinary
+  if (image) {
+  }
+};
+
 const updateItem = async (req, res) => {};
 const lostItems = async (req, res) => {};
 const foundItems = async (req, res) => {};
