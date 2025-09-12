1e22edaebd92ed5d8f69d0bde1f94df26cfe7471
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-09-12 01:16:47 +0100

    built some items route

---
+++ server/src/controllers/item.controllers.js
@@ -0,0 +1,7 @@
+const createItem = async (req, res) => {};
+const updateItem = async (req, res) => {};
+const lostItems = async (req, res) => {};
+const foundItems = async (req, res) => {};
+const deleteItem = async (req, res) => {};
+
+export { createItem, updateItem, lostItems, foundItems, deleteItem };
--- server/src/routes/items.routes.js
+++ server/src/routes/items.routes.js
@@ -1,13 +1,23 @@
-import express from express;
-
+import express from "express";
+import authMiddleWare from "../middleware/auth.js";
+import {
+  createItem,
+  updateItem,
+  lostItems,
+  foundItems,
+  deleteItem,
+} from "../controllers/item.controllers.js";
 const router = express.Router();
 
-// Post create an item
-// Patch update an item using id
-// Get /lost view lost items
-// Get /found view all found items
-// Delete an item from the DB using its ID when status == "returned"
-// 
+/* 
+router.delete("/:id")
+ Delete an item from the DB using its ID when status == "returned"
+*/
 
+router.post("/", authMiddleWare, createItem);
+router.patch("/:id", authMiddleWare, updateItem); // requires an ownership middleware
+router.get("/lost", authMiddleWare, lostItems);
+router.get("/found", authMiddleWare, foundItems);
+router.delete("/:id", authMiddleWare, deleteItem); // requires an ownership middleware
 
 export default router;
