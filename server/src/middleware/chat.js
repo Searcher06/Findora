6f3bb42c74b445cbf0f52d7754f782ccced9c9e3
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-09-25 01:05:09 +0100

    implemented sending of message between user

--- server/src/middleware/chat.js
+++ server/src/middleware/chat.js
@@ -1,18 +1,22 @@
-import mongoose from "mongoose";
 import { userModel } from "../models/user.model.js";
 import { requestModel } from "../models/request.model.js";
+
 export const chatMiddleware = async (req, res, next) => {
+  // destructuring the username from the req parameter
   const { username: userToChatUsername } = req.params;
   const { id: userId } = req.user;
 
-  const user = await userModel.findById(userId);
   const userToChat = await userModel.findOne({ username: userToChatUsername });
 
+  // if there no user found throw an error
   if (!userToChat) {
     res.status(404);
     throw new Error("User not found!");
   }
 
+  // checking the dB for any request in which the request status is accepted,
+  // and either the finder is the current user and the claimer is the user to chat
+  // or the finder is the user to chat and the claimer is the current user
   const request = await requestModel.findOne({
     status: "accepted",
     $or: [
@@ -21,10 +25,15 @@ export const chatMiddleware = async (req, res, next) => {
     ],
   });
 
+  // If the above request doesn't exist throw an error
   if (!request) {
     res.status(403);
     throw new Error(
       "You can't chat with this user, you don't have any request case"
     );
   }
+
+  req.userToChat = userToChat;
+
+  next();
 };
