e346ce05413fa7e09d66c3c2254920adc4d31931
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-17 01:09:23 +0100

    hmm

---
+++ client/src/routes/routes.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const AppRoutes = () => {
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<h1>Home page</h1>} />
+      <Route path="/login" element={<h1>Login page</h1>} />
+      <Route path="/register" element={<h1>Register page</h1>} />
+      <Route path="/chat/:id" element={<h1>chat page</h1>} />
+    </Routes>
+  </BrowserRouter>;
+};
+export default AppRoutes;
