325935ad3a123d47dbd43bc19afff15ba62ac3a5
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-18 14:14:13 +0100

    hmm

--- client/src/layouts/MainNavbar.jsx
+++ client/src/layouts/MainNavbar.jsx
@@ -1,9 +1,10 @@
 import { Menu } from "@/components/ui/menu";
-
+import { userAvatar } from "@/components/ui/userAvatar";
 export const MainNavbar = () => {
   return (
     <nav className="h-15 w-full bg-gray-200">
       <Menu />
+      <userAvatar />
     </nav>
   );
 };
