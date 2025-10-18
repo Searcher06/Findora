1c1ad02f2e469183837e772363f1d073a8cb3dd1
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-18 03:32:14 +0100

    structuring the react frontend folder structure

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,9 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Button } from "@/components/ui/button";
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home page</h1>} />
+        <Route
+          path="/"
+          element={
+            <Button className={`bg-blue-600 rounded-sm font-calibri`}>
+              Home page
+            </Button>
+          }
+        />
         <Route path="/login" element={<h1>Login page</h1>} />
         <Route path="/register" element={<h1>Register page</h1>} />
         <Route path="/chat/:id" element={<h1>chat page</h1>} />
