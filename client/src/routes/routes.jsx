f70e115101abe7fb0f8363a7aa807f62db89a9ef
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-19 22:12:27 +0100

    ..

--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,10 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { MainNavbar } from "@/layouts/MainNavbar";
+import { Home } from "@/pages/Home";
+import { AppLayout } from "@/layouts/AppLayout";
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainNavbar />} />
+        <Route element={<AppLayout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route path="/login" element={<h1>Login page</h1>} />
         <Route path="/register" element={<h1>Register page</h1>} />
         <Route path="/chat/:id" element={<h1>chat page</h1>} />
