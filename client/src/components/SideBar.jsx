a0f3323c7fa912b1ad78e0614f8f404afa5812ec
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-21 10:54:51 +0100

    ...

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -1,18 +1,40 @@
-import { ExpandIcon } from "lucide-react";
+import {
+  X,
+  LayoutDashboard,
+  SearchIcon,
+  MessageSquare,
+  Bell,
+} from "lucide-react";
+
 export const SideBar = ({ setOpenSidebar }) => {
   return (
-    <div className="h-full fixed w-60 bg-gray-300 flex pl-2 pr-2 justify-between pt-5">
-      <div className="font-normal text-[14px]">
-        <p>Dashboard</p>
-        <p>Browse</p>
-        <p>Messages</p>
-        <p>Notification</p>
+    <>
+      <div></div>
+      <div className="h-full fixed w-60 bg-white flex pl-2 pr-2 justify-between pt-5">
+        <div className="font-normal text-[13px] text-gray-700 flex-col flex gap-3">
+          <p className="flex items-center gap-1">
+            <LayoutDashboard />
+            Dashboard
+          </p>
+          <p className="flex items-center gap-1">
+            <SearchIcon /> Browse
+          </p>
+          <p className="flex items-center gap-1">
+            <MessageSquare /> Messages
+          </p>
+          <p className="flex items-center gap-1">
+            <Bell />
+            Notification
+          </p>
+        </div>
+
+        <X
+          onClick={() => {
+            setOpenSidebar(false);
+          }}
+          className="text-gray-900 text-sm"
+        />
       </div>
-      <ExpandIcon
-        onClick={() => {
-          setOpenSidebar(false);
-        }}
-      />
-    </div>
+    </>
   );
 };
