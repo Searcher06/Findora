dce382fb854ea35a32f67c1eec8f91960b32c9e5
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-24 01:18:55 +0100

    ..

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -3,27 +3,24 @@ import { NavLink } from "./ui/Links";
 import { Logo } from "./logo";
 export const SideBar = ({ setOpenSidebar }) => {
   return (
-    <>
-      <div className="h-full fixed w-60 bg-white">
-        <div className="h-14 w-full border-b-2 border-gray-100 mb-2.5">
-          <Logo className={"h-[50px]"} />
-        </div>
-        <div className="flex pl-2 pr-2 justify-between">
-          <div className="font-normal  text-[13px] text-gray-700 flex-col flex gap-3">
-            <NavLink Icon={"LayoutDashboard"} text={`Dashboard`} />
-            <NavLink Icon={"SearchIcon"} text={"Browse"} />
-            <NavLink Icon={"MessageSquare"} text={"Messages"} />
-            <NavLink Icon={"Bell"} text={"Notifications"} />
-          </div>
-
-          <X
-            onClick={() => {
-              setOpenSidebar(false);
-            }}
-            className="text-gray-900 text-sm"
-          />
+    <div className="h-full fixed w-60 bg-white transition duration-300 ease-in-out ">
+      <div className="h-14 w-full border-b-2 border-gray-100 mb-2.5 flex justify-between items-center pr-2">
+        <Logo className={``} />
+        <X
+          onClick={() => {
+            setOpenSidebar(false);
+          }}
+          className="text-gray-900 text-sm"
+        />
+      </div>
+      <div className="pl-2 pr-2">
+        <div className="font-normal  text-[13px] text-gray-700 flex-col flex gap-7">
+          <NavLink Icon={"LayoutDashboard"} text={`Dashboard`} />
+          <NavLink Icon={"SearchIcon"} text={"Browse"} />
+          <NavLink Icon={"MessageSquare"} text={"Messages"} />
+          <NavLink Icon={"Bell"} text={"Notifications"} />
         </div>
       </div>
-    </>
+    </div>
   );
 };
