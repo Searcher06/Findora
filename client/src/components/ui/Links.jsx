f8e189e0f527e03984a52d6868e8955576dcb148
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-25 06:05:46 +0100

    upadated some typography properties

--- client/src/components/ui/Links.jsx
+++ client/src/components/ui/Links.jsx
@@ -2,7 +2,7 @@ import * as Link from "lucide-react";
 export const NavLink = ({ Icon, text }) => {
   const LucideIcon = Link[Icon];
   return (
-    <p className="flex items-center gap-1">
+    <p className="flex items-center gap-1 font-sans">
       <LucideIcon />
       {`${text}`}
     </p>
