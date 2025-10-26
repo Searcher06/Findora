61a37890b8fb3661badec5ca03eb022b946e6232
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-26 01:32:35 +0100

    building the search frontend

--- client/src/features/items/pages/Browse.jsx
+++ client/src/features/items/pages/Browse.jsx
@@ -1,12 +1,14 @@
 import { Header } from "@/components/Header";
+import { SearchBar } from "../components/SearchBar";
 
 const BrowsePage = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center">
       <Header
         content={"Browse Lost and found Items"}
-        className={"text-3xl font-medium pl-4 leading-7.5 mt-2"}
+        className={"text-[26px] font-medium pl-4 pr-4 leading-7 mt-1"}
       />
+      <SearchBar className={`mt-2.5`} />
     </div>
   );
 };
