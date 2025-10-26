2f6d7a4eb31a2bb8312bcb8741f8da0866872001
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-26 12:20:38 +0100

    hmm

--- client/src/features/items/components/TabsBar.jsx
+++ client/src/features/items/components/TabsBar.jsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import { FilterMenus } from "./FilterMenus";
 
 export const TabsBar = ({ className }) => {
+  const [bar, setBar] = useState("found");
   return (
     <div
       className={`${className} font-sans w-[97%] flex justify-between pr-1 h-7 border-b`}
     >
       <div className="w-[40%] text-[13px] flex justify-between">
-        <p className="h-7 border-b-2 border-black">Lost</p>
-        <p className="h-7 border-b-2">Found</p>
+        <p
+          className={`h-7 border-b-2 ${bar == "lost" && "border-black"}`}
+          onClick={() => {
+            setBar("lost");
+          }}
+        >
+          Lost
+        </p>
+        <p
+          className={`h-7 border-b-2 ${bar == "found" && "border-black"}`}
+          onClick={() => {
+            setBar("found");
+          }}
+        >
+          Found
+        </p>
       </div>
       <FilterMenus />
     </div>
