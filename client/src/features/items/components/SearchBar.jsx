1f20a948f83ecfc08c97b715ea27f0f4d6bf6014
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-26 01:32:35 +0100

    building the search frontend

---
+++ client/src/features/items/components/SearchBar.jsx
@@ -0,0 +1,19 @@
+import { Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+export const SearchBar = ({ className }) => {
+  return (
+    <div className={`flex gap-1 ${className}`}>
+      <div className="flex items-center justify-between border-[1px] w-60 border-gray-200 rounded-sm">
+        <input
+          type="text"
+          className="w-52 h-[29px] pl-2 text-xs border-none font-sans outline-0"
+          placeholder="Search by item name, category, location"
+        />
+        <Search size={17} className="text-gray-500 pr-1" />
+      </div>
+      <Button className={`h-[29px] rounded-[3px] bg-blue-700`}>
+        <Search />
+      </Button>
+    </div>
+  );
+};
