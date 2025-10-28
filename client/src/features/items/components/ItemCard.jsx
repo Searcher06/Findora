39b397487fb67bf0d8615b9f8f02000016330b02
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-28 05:22:15 +0100

    built some frontend components

--- client/src/features/items/components/ItemCard.jsx
+++ client/src/features/items/components/ItemCard.jsx
@@ -1,11 +1,21 @@
-import { Card, CardTitle, CardDescription } from "@/components/ui/card";
-export const ItemCard = () => {
+import { Card } from "@/components/ui/card";
+import { MapPin, Calendar1Icon } from "lucide-react";
+export const ItemCard = ({ image, name, description, location, date }) => {
   return (
-    <Card className={`w-70`}>
-      <CardTitle>Findora</CardTitle>
-      <CardDescription className={`font-sans`}>
-        Findora is a lost and found app built for university campus
-      </CardDescription>
+    <Card className={`w-60`}>
+      <img src={image} alt="" className="w-full" />
+      <h1 className="font-display font-semibold leading-0">{name}</h1>
+      <p className="text-[13px] font-sans">{description}</p>
+      <div>
+        <p className="text-[13px] flex items-center">
+          <p>{<MapPin className="text-[13px] text-blue-800" />} </p>
+          <p>{location}</p>
+        </p>
+        <p className="text-[13px] flex items-center">
+          <p>{<Calendar1Icon className="text-[13px] text-blue-800" />} </p>
+          <p>{date}</p>
+        </p>
+      </div>
     </Card>
   );
 };
