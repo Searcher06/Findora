54d10c688461237e6067eca567c95f5c0dd0493d
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-10-28 11:52:21 +0100

    updated some parts of the item card

--- client/src/features/items/components/ItemCard.jsx
+++ client/src/features/items/components/ItemCard.jsx
@@ -1,21 +1,32 @@
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { MapPin, Calendar1Icon } from "lucide-react";
-export const ItemCard = ({ image, name, description, location, date }) => {
+import { MapPin, CalendarDays } from "lucide-react";
+export const ItemCard = ({
+  image,
+  name,
+  description,
+  location,
+  date,
+  status,
+}) => {
   return (
-    <Card className={`w-60`}>
+    <Card className={`w-60 pr-4 pl-4`}>
       <img src={image} alt="" className="w-full" />
       <h1 className="font-display font-semibold leading-0">{name}</h1>
-      <p className="text-[13px] font-sans">{description}</p>
-      <div>
+      <p className="text-[13px] font-sans line-clamp-2">{description}</p>
+      <div className="">
         <p className="text-[13px] flex items-center">
-          <p>{<MapPin className="text-[13px] text-blue-800" />} </p>
-          <p>{location}</p>
+          <p>{<MapPin className="text-[13px] text-white fill-blue-800" />} </p>
+          <p className="line-clamp-1">{location}</p>
         </p>
         <p className="text-[13px] flex items-center">
-          <p>{<Calendar1Icon className="text-[13px] text-blue-800" />} </p>
+          <p>{<CalendarDays className="text-[13px] text-blue-800" />} </p>
           <p>{date}</p>
         </p>
       </div>
+      <Button className={""}>
+        {status == "lost" ? "Claim" : "Mark as found"}
+      </Button>
     </Card>
   );
 };
--- client/src/features/items/components/ItemsContainer.jsx
+++ client/src/features/items/components/ItemsContainer.jsx
@@ -5,32 +5,39 @@ const items = [
     name: "Black HP Laptop",
     location: "Foculty of engineering",
     description:
-      "A black HP laptop, with 2 ports, last seen during a lecture room",
+      "A black HP laptop, with 2 ports, last seen during a lecture room,",
     date: "Oct 27,2025",
+    status: "lost",
   },
   {
     name: "Pen Drive",
     location: "Department of software engineering",
     description: "512GB sand disk pen drive, with black color",
     date: "Sep 30,2025",
+    status: "lost",
   },
   {
     name: "Thermometre",
     location: "Foculty of engineering",
     description: "A thermal thermometre, with a broken glass",
     date: "Oct 27,2025",
+    status: "found",
   },
 ];
 export const ItemsContainer = ({ className }) => {
   return (
-    <div className={`${className} w-full`}>
-      {items.map((current) => (
+    <div
+      className={`${className} w-full flex flex-wrap justify-center gap-2.5`}
+    >
+      {items.map((current, index) => (
         <ItemCard
           name={current.name}
           location={current.location}
           date={current.date}
           image={itemImage}
           description={current.description}
+          status={current.status}
+          key={index}
         />
       ))}
     </div>
