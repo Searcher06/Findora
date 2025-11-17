76d18a7e1b835a7399df8ffc4eb64ffaea918e87
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-11-17 12:02:47 +0100

    API integration of notification functionality on the frontend

--- client/src/features/verification/apis/verificationApi.js
+++ client/src/features/verification/apis/verificationApi.js
@@ -1,10 +1,22 @@
 import { Api } from "@/lib/axios";
 
-export const sendClaim = async (id) => {
-  const { data } = await Api.post(`/request/claim/${id}`);
+export const sendClaim = async (itemId) => {
+  const { data } = await Api.get(`/request/claim/${itemId}`);
   return data;
 };
-export const sendFound = async (id) => {
-  const { data } = await Api.post(`/request/found/${id}`);
+export const sendFound = async (itemId) => {
+  const { data } = await Api.get(`/request/found/${itemId}`);
+  return data;
+};
+export const sendQuestion = async (requestId, questions) => {
+  const { data } = await Api.get(`/verify/setquestion/${requestId}`, questions);
+  return data;
+};
+export const sendAnswers = async (requestId, questions) => {
+  const { data } = await Api.get(`/verify/setanswers/${requestId}`, questions);
+  return data;
+};
+export const sendDecision = async (requestId, decision) => {
+  const { data } = await Api.get(`/verify/${requestId}`, decision);
   return data;
 };
