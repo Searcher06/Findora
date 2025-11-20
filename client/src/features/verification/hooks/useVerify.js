ca208b82d8ffdc0f91d1bca3ae6946b671a74a01
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-11-20 22:05:40 +0100

    Integrating the backend API for send of verification questions

--- client/src/features/verification/hooks/useVerify.js
+++ client/src/features/verification/hooks/useVerify.js
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { sendClaim, sendFound } from "../apis/verificationApi";
+import {
+  sendClaim,
+  sendFound,
+  sendQuestion,
+  sendAnswers,
+  sendDecision,
+} from "../apis/verificationApi";
 export const useVerify = () => {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState(null);
@@ -30,6 +36,47 @@ export const useVerify = () => {
       setLoading(false);
     }
   };
+
+  const sendVerificationQuestions = async (requestId, questions) => {
+    try {
+      setLoading(true);
+      const data = await sendQuestion(requestId, questions);
+      setData(data);
+    } catch (error) {
+      setError(error.response?.data?.message || "failed to send questions");
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendVerificationAnswers = async (requestId, answers) => {
+    try {
+      setLoading(true);
+      const data = await sendAnswers(requestId, answers);
+      setData(data);
+    } catch (error) {
+      setError(error.response?.data?.message || "failed to send answers");
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const sendVerificationDecision = async (requestId, decision) => {
+    try {
+      setLoading(true);
+      const data = await sendDecision(requestId, decision);
+      setData(data);
+    } catch (error) {
+      setError(
+        error.response?.data?.message || "failed to send final decision"
+      );
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
   return {
     loading,
     data,
@@ -37,5 +84,8 @@ export const useVerify = () => {
     sendClaimRequest,
     sendFoundRequest,
     setLoading,
+    sendVerificationQuestions,
+    sendVerificationAnswers,
+    sendVerificationDecision,
   };
 };
--- client/src/features/verification/pages/GenerateQuestions.jsx
+++ client/src/features/verification/pages/GenerateQuestions.jsx
@@ -1,5 +1,14 @@
+import { Header } from "@/components/Header";
+
 const GenerateQuestions = () => {
-  return <div className="mt-14 font-sans">GenerateQuestions</div>;
+  return (
+    <div className="mt-14 font-sans pl-2 pr-2 w-full">
+      <Header className={"text-xl pt-2 text-center"}>
+        Create Verification Questions
+      </Header>
+      <div className="mt-1 w-[90%] h-1 bg-blue-600 rounded-full"></div>
+    </div>
+  );
 };
 
 export default GenerateQuestions;
