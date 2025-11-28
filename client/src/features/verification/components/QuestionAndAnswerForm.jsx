5ff531a3be53c8ec1026302c45abf8be4e471795
Searcher06 <ahmadibrahimsearcher@gmail.com>
2025-11-28 10:49:05 +0100

    feat:Added skeleton display when fetching of questions on the answer questions page

--- client/src/features/verification/components/QuestionAndAnswerForm.jsx
+++ client/src/features/verification/components/QuestionAndAnswerForm.jsx
@@ -1,4 +1,5 @@
 import { InputsForm } from "./InputsForm";
+import { InputsFormSkeleton } from "./InputsFormSkeleton"; // Import the skeleton
 
 export const QuestionAndAnswerForm = ({
   className,
@@ -9,10 +10,18 @@ export const QuestionAndAnswerForm = ({
   requestId,
 }) => {
   if (requestLoading) {
-    return <p>Fetching...</p>;
+    return (
+      <div className={`${className} w-full`}>
+        <h1 className="font-sans text-[13px] font-bold text-gray-900">
+          Answer All Questions
+        </h1>
+        <InputsFormSkeleton className={"mt-2"} count={3} />
+      </div>
+    );
   } else if (requestError) {
     return <p>{requestError}</p>;
   }
+
   return (
     <div className={`${className} w-full`}>
       <h1 className="font-sans text-[13px] font-bold text-gray-900">
