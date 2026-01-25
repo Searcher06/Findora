1836270d6f4ae059273e941fdc224a6c267ef27e
Ahmad Ibrahim <ahmadibrahimsearcher@gmail.com>
2026-01-25 20:55:50 +0100

    Made some changes on the navbar

--- client/src/components/userAvatar.jsx
+++ client/src/components/userAvatar.jsx
@@ -1,8 +1,14 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 import avatarimage from "../constants/avatar2.jpg";
+import { useNavigate } from "react-router-dom";
 export const UserAvatar = () => {
+  const navigate = useNavigate();
   return (
-    <Avatar>
+    <Avatar
+      onClick={() => {
+        navigate("/profile");
+      }}
+    >
       <AvatarImage src={`${avatarimage}`} />
       <AvatarFallback>AI</AvatarFallback>
     </Avatar>
--- client/src/features/user/services/api.js
+++ client/src/features/user/services/api.js
@@ -1,6 +1,5 @@
 import { Api2 } from "@/lib/axios";
 export const updateProfile = async (updatedFields) => {
   const { data } = await Api2.post("/user/profile", updatedFields);
-  console.log("Debug 0:", data);
   return data;
 };
