7fd8976553b758aae49cd67527f8cb9b5ed85ce9
Ahmad Ibrahim <ahmadibrahimsearcher@gmail.com>
2026-02-19 05:04:23 +0100

    Fixed:resend bug

--- server/src/utils/sendEmail.js
+++ server/src/utils/sendEmail.js
@@ -2,12 +2,14 @@ import { resend } from "../config/resend.js";
 
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-    return await resend.emails.send({
+    const send = await resend.emails.send({
       from: "Findora Support <onboarding@resend.dev>",
       to,
       subject,
       html,
     });
+    console.log(send);
+    return send;
   } catch (err) {
     console.error("Email failed:", err);
     throw err;
